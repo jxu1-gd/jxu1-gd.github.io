4a474205732430fba0b1b15bfc983c0f144385b3
Jack Xu <jxu1@greendotcorp.com>
2022-06-07 19:20:28 +0800

    update build

---
+++ static/js/854.d99c5bf5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktfin_ui_project_template=self.webpackChunktfin_ui_project_template||[]).push([[854],{481:function(t,e,u){u.r(e);var n=u(495),c=u(184);e.default=function(){return(0,c.jsx)(n.Z,{})}}}]);
+//# sourceMappingURL=854.d99c5bf5.chunk.js.map
